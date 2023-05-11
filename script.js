0672d3f0701884e812a9bad07ede7c867079d031
brandon <branthebuilder321@gmail.com>
2023-05-11 18:20:58 -0400

    Add boilerplate function declaration from TOP

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+
 function getComputerChoice() {
 
     let number = Math.floor(Math.random() * 3);
@@ -13,3 +17,7 @@ function getComputerChoice() {
             return "scissors";
     }
 }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
